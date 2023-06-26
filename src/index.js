432424a12a9f1d8afbc5ccbd372a44cf0beb499a
hadi7325 <hadyjmaly184@gmail.com>
2023-06-26 14:43:27 +0430

    update

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <AppProvider>
+    {/* <AppProvider>
     <Router>
      
     </Router>
-    </AppProvider>
+    </AppProvider> */}
+    <>laksddjlasjkl</>
   
   </React.StrictMode>
 );
